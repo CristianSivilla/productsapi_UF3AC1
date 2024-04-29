c61e1f0c5d16fbb5563e6026e7e306edb33cd1f0
CristianSivilla <cristiansivillacalvo@gmail.com>
2024-04-29 19:39:11 +0200

    commit1

--- config/config.js
+++ config/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: ""
+  mongoURI: "mongodb+srv://cristiansivillacalvo:cJGYUctB3Zpg2Y9l@cluster0.dtqb5ug.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
 };
