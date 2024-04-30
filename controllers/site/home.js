631235c40b2eb80792e7ddbaf88904ae904e216d
CristianSivilla <cristiansivillacalvo@gmail.com>
2024-04-30 17:05:57 +0200

    .

--- controllers/site/home.js
+++ controllers/site/home.js
@@ -1,25 +1,23 @@
-const slugify = require('slugify');
-
-const renderHome = (req, res) => {
-    titulo = "Welcome to Products API";
-    aviso = "Register to get API key!";
-    howto = "(to provide it, place it on header 'api-key' or as URL param 'apikey')";
+const renderHome = (req,res) => {
+    titulo = "Wellcome to Products API"
+    aviso = "Register to get API key!"
+    howto = "(to provide it, place it on header 'api-key' or as URL param 'apikey')"
     rutas = [
-        ["Register", "POST /api/users/register"],
-        ["List all products", "GET /api/products"],
-        ["List products by ID", "GET /api/products/:productID"],
-        ["Create product", "POST /api/products"],
-        ["Update product", "PUT /api/products/:productID"],
-        ["Delete product", "DELETE /api/products/:productID"]
-    ];
-
-    const welcomeSlug = slugify(titulo, { replacement: '*', lower: true });
-
-    res.render('index.pug', {
-        titulo: welcomeSlug,
+        ["Register","POST /api/users/register"],
+        ["List all products","GET /api/products"],
+        ["List products by ID","GET /api/products/:productID"],
+        ["Create product","POST /api/products"],
+        ["Update product","PUT /api/products/:productID"],
+        ["Delete product","DELETE /api/products/:productID"]
+    ]
+  
+    res.render('index.pug',
+    {  
+        titulo: titulo,
         aviso: aviso,
         rutas: rutas
-    });
-};
+    })
+}
 
-module.exports = renderHome;
+module.exports = renderHome
+
